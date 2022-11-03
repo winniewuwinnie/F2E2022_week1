521852dc7150d45bb9d616fbd3bba808cca2db5c
Winnie <tzuhsuan1999@gmail.com>
2022-11-03 18:22:09 +0800

    新增環境

--- javascript/all.js
+++ javascript/all.js
@@ -1,6 +1,19 @@
-import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin.js";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+// import { gsap } from "gsap";
+// import { PixiPlugin } from "gsap/PixiPlugin.js";
+// import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
 
-//without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
-gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
+// //without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
+// gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
+gsap.registerPlugin(ScrollTrigger, TextPlugin);//註冊我們額外引入的 ScrollTrigger 與 TextPlugin 套件
+
+const srollTL=gsap.timeline({
+  scrollTrigger:{
+    trigger:".section-week",
+    pin:true,//重要! pin虛設為true
+    markers:true,
+    scrub:true,
+  }
+});
+
+srollTL.to(".week1", { xPercent: "-100" });
+srollTL.to(".week2",{xPercent:"-100"});
