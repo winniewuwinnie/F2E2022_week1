cfe7af347efc932e9917f22a05891b8862920a1f
Winnie <tzuhsuan1999@gmail.com>
2022-11-03 14:42:14 +0800

    新增環境

---
+++ javascript/all.js
@@ -0,0 +1,6 @@
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+
+//without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
+gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
